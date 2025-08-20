ab4ce8cb5a1ef2d75301ac3e25a2811d40ca77d0
JeRoch4021 <161788973+JeRoch4021@users.noreply.github.com>
2025-08-19 19:21:15 -0600

    New changes

--- src/components/PokemonThumbnail/PokemonThumbnail.tsx
+++ src/components/PokemonThumbnail/PokemonThumbnail.tsx
@@ -1,31 +1,60 @@
+import { useNavigate } from "@tanstack/react-router";
+
 interface PokemonThumbnailProps {
-    name: string;
-    id: number;
-    imageUrl?: string;
-    isLoading?: boolean;
+  name: string;
+  id: number;
+  imageUrl?: string;
+  isLoading?: boolean;
 }
 
-function CompleteId (id: number): string {
-  return `#${id.toString().padStart(3, '0')}`;
+function CompleteId(id: number): string {
+  return `#${id.toString().padStart(3, "0")}`;
 }
 
 export const PokemonThumbnail = (props: PokemonThumbnailProps) => {
-  const { name, id, imageUrl, isLoading} = props;
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate({
+      to: "/details",
+    });
+  };
+
+  const { name, id, imageUrl, isLoading } = props;
   if (isLoading) {
-    <button className="relative border rounded-sm h-40 flex flex-col justify-end items-center p-2
-                       hover:bg-gray-100 active:scale-95 transition-transform">
+    <button
+      className="relative border rounded-sm h-40 flex flex-col justify-end items-center p-2
+                       hover:bg-gray-100 active:scale-95 transition-transform"
+    >
       <label className="absolute top-1 right-1 text-xs font-normal text-gray-600"></label>
-      {imageUrl && (<img src={imageUrl} alt={name} className="w-30 h-30 object-contain mt-4"/>)}
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={name}
+          className="w-30 h-30 object-contain mt-4"
+        />
+      )}
       <label className="text-base font-medium text-gray-600"></label>
-    </button>
+    </button>;
   }
 
   return (
-    <button className="relative border rounded-sm h-40 flex flex-col justify-end items-center p-2
-                       hover:bg-gray-100 active:scale-95 transition-transform">
-      <label className="absolute top-1 right-1 text-xs font-normal text-gray-600">{CompleteId(id)}</label>
-      {imageUrl && (<img src={imageUrl} alt={name} className="w-30 h-30 object-contain mt-4"/>)}
+    <button
+      className="relative border rounded-sm h-40 flex flex-col justify-end items-center p-2
+                       hover:bg-gray-100 active:scale-95 transition-transform"
+      onClick={handleOnClick}  
+    >
+      <label className="absolute top-1 right-1 text-xs font-normal text-gray-600">
+        {CompleteId(id)}
+      </label>
+      {imageUrl && (
+        <img
+          src={imageUrl}
+          alt={name}
+          className="w-30 h-30 object-contain mt-4"
+        />
+      )}
       <label className="text-base font-medium text-gray-600">{name}</label>
     </button>
   );
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,51 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './main.css'
-import { PokemonList } from './pages/PokemonList/PokemonList.tsx'
+// import { PokemonList } from './pages/PokemonList/PokemonList.tsx'
 import { QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
+import {
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+  redirect
+} from '@tanstack/react-router'
+import { PokemonList } from './pages/PokemonList/PokemonList'
+import { PokemonDetails } from './pages/PokemonDetails/PokemonDetails'
+
+const pokemonRoute = createRootRoute({
+  component: () => <Outlet />,
+})
+
+const pokemonListRoute = createRoute({
+  getParentRoute: () => pokemonRoute,
+  path: '/',
+  component: PokemonList,
+})
+
+const pokemonDetailRoute = createRoute({
+  getParentRoute: () => pokemonRoute,
+  path: '/details',
+  component: PokemonDetails,
+})
+
+const routePokemonTree = pokemonRoute.addChildren([
+  pokemonListRoute,
+  pokemonDetailRoute,
+]);
+
+const routerPokemon = createRouter({ routeTree: routePokemonTree });
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <PokemonList />
+      <>
+        <RouterProvider router={routerPokemon} />
+      </> 
     </QueryClientProvider>
   </StrictMode>,
 )
